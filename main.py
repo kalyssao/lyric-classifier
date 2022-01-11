a0f60636dfecb0cf0dc90aa0b1efe69731c02f6b
kalyssao <klysssa@icloud.com>
2022-01-11 16:25:05 +0000

    infra for vectorizing and making predictions

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pickle
+import string
+import pandas as pd
 
 
 def preprocessLyric(lyric):
-    return lyric
+    sans_punctuation = lyric.translate(str.maketrans("", "", string.punctuation))
+    lower = sans_punctuation.lower()
+
+    lyric_df = pd.DataFrame([lower], columns=["lyric"])
+
+    with open("vectorizer", "rb") as f:
+        vectorizer = pickle.load(f)
+
+    vectorized = vectorizer.transform(lyric_df)
+    return vectorized
 
 
-def predoctGivenLyric(lyric):
+def predictGivenLyric(lyric):
     preprocessed_lyric = preprocessLyric(lyric)
-    loaded_model = pickle.load(open("lyric_classifier.sav", "rb"))
+    loaded_model = pickle.load(open("svm_model", "rb"))
 
     result = loaded_model.predict(preprocessed_lyric)
-    if result == 1:
+    if result == 0:
         print("Davido") # do davido processing here
     else:
         print("Wizkid") # send Davido
